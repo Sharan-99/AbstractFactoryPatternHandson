a432bb61c75b84c409a80b1c5ccd128c9e29d7c0
Sharan-99 <sharan.ceg2017103591@gmail.com>
2022-04-26 10:41:45 +0530

    first commit

---
+++ AutomobileClient/AutomobileClient/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using AutomobileManufacturer;
+
+namespace AutomobileClient
+{
+    class Program
+    {
+        static Factory GetFactory(string factoryName)
+        {
+            Assembly assembly = typeof(Factory).Assembly;
+            object instance = assembly.CreateInstance(factoryName);
+            return instance as Factory;
+        }
+        static void Main(string[] args)
+        {
+            //Validating output using MercedesFactory
+            Factory factory = GetFactory("AutomobileManufacturer.MercedesFactory");
+            Headlight h = factory.MakeHeadlight();
+            Tire t = factory.MakeTire();
+            h.ManufacturedHeadlight();
+            t.ManufacturedTire();
+
+            Console.WriteLine("-------------------------");
+
+            //Validating output using AudiFactory
+            Factory factory1 = GetFactory("AutomobileManufacturer.AudiFactory");
+            Headlight h1 = factory1.MakeHeadlight();
+            Tire t1 = factory1.MakeTire();
+            h1.ManufacturedHeadlight();
+            t1.ManufacturedTire();
+
+            Console.ReadKey();
+
+        }
+    }
+}
